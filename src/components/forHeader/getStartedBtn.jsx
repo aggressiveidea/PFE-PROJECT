fa679cf598cd5f94fa555a53e9f08d96a4bb71c5
imessaoudene_aldjia <150512088+aggressiveidea@users.noreply.github.com>
2025-03-05 21:25:06 +0100

    initializing the header components

---
+++ src/components/forHeader/getStartedBtn.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function getStartedBtn() {
+  return (
+    <div>
+        <button className=''>
+            Get started
+        </button>
+    </div>
+  )
+}
+
+export default getStartedBtn
